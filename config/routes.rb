d3f27111685aefd6bbc397712637a00f4ab956cc
akash chhetri <akash.chhetri1@gmail.com>
2021-04-20 09:53:04 +1000

    routes for the api added and airline serliazer relationship added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, params: :slug
+      resources :reviews, only [:create, :destroy]
+    end
+  end
+
+  #all path diverted to home page
+  get '*path', to: 'pages#index', via: :all
 end
