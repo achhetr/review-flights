8dffa87167b26479f6223272948bffc1c3c5805c
akash chhetri <akash.chhetri1@gmail.com>
2021-04-20 11:43:24 +1000

    forgery protected removed from the rails api protection

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :airlines, params: :slug
-      resources :reviews, only [:create, :destroy]
+      resources :reviews, only: [:create, :destroy]
     end
   end
 
-  #all path diverted to home page
+  #all unmatched route path diverted to home page
   get '*path', to: 'pages#index', via: :all
 end
