3dd76b019a59defad1b54a653022216f351b6255
akash chhetri <akash.chhetri1@gmail.com>
2021-04-20 13:29:32 +1000

    styled component added to the Airline and Airlines component

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,39 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
+import styled from "styled-components";
 import Airline from "./Airline";
+
+const Home = styled.div`
+	text-align: center;
+	margin-left: auto;
+	margin-right: auto;
+	max-width: 1200px;
+`;
+
+const Grid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	grid-gap: 20px;
+	width: 100%;
+	padding: 20px;
+	> div {
+		background-color: #fff;
+		border-radius: 5px;
+		padding: 20px;
+	}
+`;
+const Header = styled.div`
+	padding: 100px 100px 10px 100px;
+
+	h1 {
+		font-size: 42px;
+	}
+`;
+
+const Subheader = styled.p`
+	font-weight: 300;
+	font-size: 26px;
+`;
 
 const Airlines = () => {
 	const [airlines, setAirlines] = useState([]);
@@ -24,15 +57,13 @@ const Airlines = () => {
 	});
 
 	return (
-		<div className="home">
-			<div className="header">
+		<Home>
+			<Header>
 				<h1>Review Flights</h1>
-				<div className="subheader">Honest review by the passengers</div>
-			</div>
-			<div className="grid">
-				<ul>{grid}</ul>
-			</div>
-		</div>
+				<Subheader>Honest review by the passengers</Subheader>
+			</Header>
+			<Grid>{grid}</Grid>
+		</Home>
 	);
 };
 
